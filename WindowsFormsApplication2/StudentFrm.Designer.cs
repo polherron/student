452ea367d2713370ebae283d32e2411e06932ee5
Paul Herron <pol.herron@gmail.com>
2017-10-21 23:27:32 +0100

    validation added

--- WindowsFormsApplication2/StudentFrm.Designer.cs
+++ WindowsFormsApplication2/StudentFrm.Designer.cs
@@ -43,56 +43,63 @@
             // 
             // txtFname
             // 
-            this.txtFname.Location = new System.Drawing.Point(103, 174);
+            this.txtFname.Location = new System.Drawing.Point(137, 214);
+            this.txtFname.Margin = new System.Windows.Forms.Padding(4);
             this.txtFname.Name = "txtFname";
-            this.txtFname.Size = new System.Drawing.Size(156, 20);
+            this.txtFname.Size = new System.Drawing.Size(207, 22);
             this.txtFname.TabIndex = 0;
             // 
             // txtDOB
             // 
-            this.txtDOB.Location = new System.Drawing.Point(103, 243);
+            this.txtDOB.Location = new System.Drawing.Point(137, 299);
+            this.txtDOB.Margin = new System.Windows.Forms.Padding(4);
             this.txtDOB.Name = "txtDOB";
-            this.txtDOB.Size = new System.Drawing.Size(156, 20);
+            this.txtDOB.Size = new System.Drawing.Size(207, 22);
             this.txtDOB.TabIndex = 1;
             // 
             // txtSNumber
             // 
-            this.txtSNumber.Location = new System.Drawing.Point(103, 279);
+            this.txtSNumber.Location = new System.Drawing.Point(137, 343);
+            this.txtSNumber.Margin = new System.Windows.Forms.Padding(4);
             this.txtSNumber.Name = "txtSNumber";
-            this.txtSNumber.Size = new System.Drawing.Size(156, 20);
+            this.txtSNumber.Size = new System.Drawing.Size(207, 22);
             this.txtSNumber.TabIndex = 2;
             // 
             // txtSname
             // 
-            this.txtSname.Location = new System.Drawing.Point(103, 209);
+            this.txtSname.Location = new System.Drawing.Point(137, 257);
+            this.txtSname.Margin = new System.Windows.Forms.Padding(4);
             this.txtSname.Name = "txtSname";
-            this.txtSname.Size = new System.Drawing.Size(156, 20);
+            this.txtSname.Size = new System.Drawing.Size(207, 22);
             this.txtSname.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 174);
+            this.label1.Location = new System.Drawing.Point(55, 214);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(57, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "F Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 216);
+            this.label2.Location = new System.Drawing.Point(55, 266);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "S Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 250);
+            this.label3.Location = new System.Drawing.Point(55, 308);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 6;
             this.label3.Text = "DOB";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -100,26 +107,30 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(41, 286);
+            this.label4.Location = new System.Drawing.Point(55, 352);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.Size = new System.Drawing.Size(71, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "S Number";
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(103, 72);
+            this.btnAdd.Location = new System.Drawing.Point(137, 162);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
             this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnFile
             // 
-            this.btnFile.Location = new System.Drawing.Point(203, 72);
+            this.btnFile.Location = new System.Drawing.Point(257, 162);
+            this.btnFile.Margin = new System.Windows.Forms.Padding(4);
             this.btnFile.Name = "btnFile";
-            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.Size = new System.Drawing.Size(100, 28);
             this.btnFile.TabIndex = 9;
             this.btnFile.Text = "button1";
             this.btnFile.UseVisualStyleBackColor = true;
@@ -131,9 +142,9 @@
             // 
             // frmStudent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 404);
+            this.ClientSize = new System.Drawing.Size(540, 497);
             this.Controls.Add(this.btnFile);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label4);
@@ -144,6 +155,7 @@
             this.Controls.Add(this.txtSNumber);
             this.Controls.Add(this.txtDOB);
             this.Controls.Add(this.txtFname);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmStudent";
             this.Text = "Students";
             this.Load += new System.EventHandler(this.Form1_Load);
---
+++ WindowsFormsApplication2/StudentFrm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication2
+{
+
+    public partial class frmStudent : Form
+    {
+        string filePath = string.Empty;
+        string studentXmlFile = string.Empty;
+
+        public frmStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (validateForm())
+                    {
+                        XElement xelement = XElement.Load(openFileDialog1.FileName);
+                        //IEnumerable<XElement> student = xelement.Elements();
+
+                        int studentIdMatch = (from idNo in xelement.Elements("Student")
+                                              where (string)idNo.Element("studentID") == txtSNumber.Text
+                                              select idNo.Element("studentID")).Count();
+                        if (studentIdMatch != 0)
+                        {
+                            MessageBox.Show("Error: A student with this student ID already exists on file");
+                        }
+                        else
+                        {
+
+                            xelement.Add(new XElement("Student",
+                                new XElement("FName", txtFname.Text),
+                                new XElement("SName", txtSname.Text),
+                                new XElement("studentID", txtSNumber.Text),
+                                new XElement("DOB", txtDOB.Text)));
+                            xelement.Save(openFileDialog1.FileName);
+                            MessageBox.Show(String.Format("Student {0} {1} has been added", txtFname.Text, txtSname.Text), "Student Details Entered");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private bool validateForm()
+        {
+            return (validateFName() && validateSName() && validateDate());
+        }
+
+        private bool validateFName()
+        {
+            bool fNameOk = (txtFname.Text.Length >= 0 && txtFname.Text.Length <= 20);
+            if (!fNameOk)
+            {
+                MessageBox.Show("First name must be bewtween 1 and 20 characters", "Name Error");
+            }
+            return fNameOk;
+        }
+
+        private bool validateSName()
+        {
+            bool SNameOk = (txtSname.Text.Length > 0 && txtSname.Text.Length < 20);
+            if (!SNameOk)
+            {
+                MessageBox.Show("Last name must be bewtween 1 and 20 characters", "Name Error");
+            }
+            return SNameOk;
+        }
+
+        private bool validateDate()
+        {
+            DateTime dt;
+            if (!DateTime.TryParse(txtDOB.Text, out dt))
+            {             
+                MessageBox.Show("Date must be in the format dd/mm/yyyy", "Date Error");
+            }
+            return (DateTime.TryParse(txtDOB.Text, out dt));
+        }
+        private bool validateMod11()
+        {
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
